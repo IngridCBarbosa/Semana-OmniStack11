3bd8489b8b294b4567736aa6b4d9e4b6680d0656
INGRID <ingridcarolinelima@hotmail.com>
2020-03-27 15:01:02 -0300

    adicionado funcoes que interagem com o banco

--- frontend/src/Pages/Logon/index.js
+++ frontend/src/Pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom'
 import {FiLogIn} from 'react-icons/fi'
+
+import Api from '../../services/api'
 
 import './style.css'
 
@@ -8,15 +10,36 @@ import logoImg from '../../assets/logo.svg'
 import heroesImg  from '../../assets/heroes.png'
 
 export default function Logon(){
+
+    const [id, setId] = useState('')
+    const history = useHistory()
+
+    async function handleLogin(e) {
+        e.preventDefault()
+
+        try{
+            const resp = await Api.post('session',{ id })
+            localStorage.setItem('ongId',id)
+            localStorage.setItem('ongName', resp.data.name)
+            history.push('profile')
+        }catch(err){
+            alert('Falha no login, tente novemente')
+        }
+    }
+
     return(
         <div className="logon-container">
            <section className="form">
                 <img src={logoImg} alt="Be The Hero" />
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Sua ID"/>
+                    <input 
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button className="button" type="submit">Entrar</button>
 
                     
